31627a680cbddaf74b1dfdd6670ab3eeaaa07f7b
Joe Evans <josephe@andrew.cmu.edu>
2018-03-23 11:14:18 -0400

    remove old file

---
+++ code/class/cmu/ddd/directory/application/services/AddpeopleService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace cmu\ddd\directory\application\services;
+
+use cmu\ddd\directory\domain\model\actors\people\People;
+use cmu\ddd\directory\infrastructure\services\dto\DTO;
+use cmu\ddd\directory\infrastructure\domain\model\factory\AbstractPersistenceFactory;
+use cmu\ddd\directory\infrastructure\domain\model\DomainObjectAssembler;
+use cmu\ddd\directory\infrastructure\domain\model\idobject\PeopleIdentityObject;
+
+class GetpeopleService extends AbstractService 
+
+{
+
+	public function execute(DTO $dto, string $action) : DTO
+
+	{
+
+
+
+
+	}
+
+
+
+
+}
