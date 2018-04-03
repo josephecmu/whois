41851a9a97572b7baf4b0baf7877f64c5722f549
Joe Evans <josephe@andrew.cmu.edu>
2018-04-03 12:22:25 -0400

    small updates

--- code/class/cmu/ddd/directory/application/services/people/UpdatePeopleService.php
+++ code/class/cmu/ddd/directory/application/services/people/UpdatePeopleService.php
@@ -10,13 +10,10 @@ class UpdatePeopleservice extends AbstractPeopleService
 	public function execute(DTO $dto) : bool
 	{
 
-		////some checking confirming the a userID does not already exist	
-		//identity generator reference here??????
-		//also check for existance of gidnumber and uidnumber and homedirectory
-
 		//we need to get the Andrewid from DN
-		$dn = $dto->get('dn');					//returns uid=jacke,ou=people,dc=mcs,dc=cmu,dc=edu
-    	$andrewid = substr(substr($dn, 0, strpos( $dn, ',', 0)), 4);        //get to first comma, then strip 'uid='
+		$dn = $dto->get('dn');										//returns uid=jacke,ou=people,dc=mcs,dc=cmu,dc=edu
+
+		$andrewid =  $this->getUid($dn);
 
 		$dto->set('andrewid', $andrewid);
 
