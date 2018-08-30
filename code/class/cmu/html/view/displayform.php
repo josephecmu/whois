8f96aa08ef124294b1f0b3017eedb54f67826c35
Joe Evans <josephe@andrew.cmu.edu>
2018-08-30 14:29:54 -0400

    prep for screencast

--- code/class/cmu/html/view/displayform.php
+++ code/class/cmu/html/view/displayform.php
@@ -10,21 +10,18 @@
 
         if ($control->checkChangeForm()) {                 //see if the form has changed before we go any further!!
   
-//            try {
+  			try {
 
             $control->processForm();
             
-                $this->forward( '../code/class/cmu/html/view/success.php' );
+//                $this->forward( '../code/class/cmu/html/view/success.php' );
 //				$this->forward ( 'results.php' );
-//			header ('Location: http://whois.math.cmu.edu/results.php');				
+				header ('Location: http://whois.math.cmu.edu');				
 
+      		} catch ( \Exception $e ) {
+		   		$this->forward( '../code/class/cmu/html/view/error.php' );
 
-
-  //      } catch ( \Exception $e ) {
-
-//		   $this->forward( '../code/class/cmu/html/view/error.php' );
-
-//		 }
+		 }
 
       } 
 
--- public_html/index.php
+++ public_html/index.php
@@ -37,6 +37,8 @@ ul {
 <ul>
 <li><a href="http://whois.math.cmu.edu/results.php">Users</a></li>
 <li><a href="http://whois.math.cmu.edu/form.php?ou=ou=people">Create New User</a></li>
+<li><a href="http://whois.math.cmu.edu/form.php?ou=ou=rooms">Create New Room</a></li>
+<li><a href="http://whois.math.cmu.edu/form.php?dn=cn=Wh6102,ou=rooms,dc=mcs,dc=cmu,dc=edu">Edit Room 6102</a></li>
 </ul>
 </div>
 </div?
